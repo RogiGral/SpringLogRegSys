80272275c10628184aee42758ccb32e4c5d40a9b
RogiGral <igor.gralak99@gmail.com>
2021-07-16 18:31:08 +0200

    initial commit

---
+++ src/main/java/com/example/springlogregsys/SpringLogRegSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlogregsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogRegSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLogRegSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springlogregsys/SpringLogRegSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlogregsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLogRegSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
